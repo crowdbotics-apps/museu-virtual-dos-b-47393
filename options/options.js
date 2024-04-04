46662689d5f4a74edc256fcb719db2045795d6d3
Crowdbotics <team@crowdbotics.com>
2024-04-04 23:24:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "museu-virtual-dos-b-47393",
+  url: "https://museu-virtual-dos-b-47393.botics.co",
+  api: "https://museu-virtual-dos-b-47393.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
